77f004ced6b4c7f05df364663e2f85ef157d0df1
beelp <beelphegor31@gmail.com>
2023-11-14 03:30:05 -0300

    try to fix .gitignore

--- app/src/main/java/com/example/workspace2/Login.java
+++ app/src/main/java/com/example/workspace2/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,7 @@ public class Login extends AppCompatActivity {
         btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("LoginActivity", "Presionaste el botón Ingresar");
                 Intent intent = new Intent(Login.this,MainActivity.class);
                 startActivity(intent);
             }
